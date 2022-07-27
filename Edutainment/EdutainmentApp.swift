5b6fa3679d39bba5e472d74d84f2d92e48f98e9f
Burak Cüce <b.cuece@web.de>
2022-07-27 16:41:01 +0200

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Burak Cüce on 27.07.22.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
